373554a02271772f796a8129c5b90872bdb30fd5
Satoshi Fujinaga <b.fujinaga@gmail.com>
2016-09-21 09:38:04 +0900

    Singleton & phpinfo

--- app/singleton/index.php
+++ app/singleton/index.php
@@ -13,6 +13,13 @@ echo "<pre>";
 echo Test::$nuga;
 echo "</pr>";
 
+echo "\ngetVar\n";
+$test->setVar();
+echo "<pre>";
+echo $test->getVar();
+echo "</pr>";
+
+
 // $test2 = new Test();
 $test2 = Test::getInstance();
 echo "<pre>";
@@ -25,19 +32,46 @@ echo "<pre>";
 echo Test::$nuga;
 echo "</pr>";
 
+echo "ngetVar\n";
+$test2->setVar();
+echo "<pre>";
+echo $test2->getVar();
+echo "</pr>";
+
 echo "<pre>";
 var_dump($test === $test2);
 echo "</pr>";
+
+/*
+$test3 = clone $test2;
+echo "<pre>";
+echo $test2->fuga;
+echo "</pr>";
+echo "<pre>";
+echo $test3->getVar();
+echo "</pr>";
+
+echo "<pre>";
+var_dump($test3 === $test2);
+echo "</pr>";
+echo "<pre>";
+print_r($test2);
+echo "</pr>";
+echo "<pre>";
+print_r($test3);
+echo "</pr>";
+*/
 
 class Test
 {
 	private static $instance;
 	public static $nuga;
 	private $hoge;
+	private static $var;
 	public $fuga;
 	public static function getInstance()
 	{
-		self::$instance = "hoge";
+		// self::$instance = "hoge";
 		if (!self::$instance instanceof Test){
 		// if (!isset(self::$instance)){
 			self::$instance = new Test();
@@ -56,5 +90,21 @@ class Test
 	public function getHoge()
 	{
 		return $this->hoge;
+	}
+
+	public static function getVar()
+	{
+		return self::$var;
+	}
+
+	public function setVar()
+	{
+		self::$var++;
+	}
+
+	public final function __clone()
+	{
+		// var_dump(get_class($this));
+		throw new RuntimeException(get_class($this));
 	}
 }
